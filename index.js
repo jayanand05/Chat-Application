0e0d4333ca9a789fc454dcae8590350a3a34f516
JAY ANAND <jay99628@gmail.com>
2024-07-09 23:57:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);hshhs
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
