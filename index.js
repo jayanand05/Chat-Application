4cb8235fe39a446435d5dd6121f74c4e12a80a6f
JAY ANAND <jay99628@gmail.com>
2024-07-10 23:58:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();
+const app = express();gg
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
